a440ca4d7aa127cb04ed777892e57b66737470d8
kmstubbs <kmstubbs@gmail.com>
2015-10-25 00:45:35 -0400

    Update main.cpp

--- SampleCode/PublicationProcessing_final/main.cpp
+++ SampleCode/PublicationProcessing_final/main.cpp
@@ -10,24 +10,7 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-
-    /*
-     * At present,
-     * 1. csv is loaded into a vector of string vectors "data" by parsetestFTN (a temporary function)
-     * 2. "data" is processed during the creation of PublicationProcessing Object
-     * 3. queries to the data may be made for all, published, submitted, and accepted entries
-     * 4. a specific type (e.g., Journal Article) may be queried by including the type's index in the string
-     *    vector returned by PublicationProcessing.getTypes()
-     *
-     * NOTE:
-     * -At present, only authors and counts may be queried
-     * -My intention is for other processing Objects (e.g., PresentationsProcessing) to also extend the CSVProcessing Object
-     * -CSVProcessing contains helper functions which should make further functions much easier to implement
-     * -Function descriptions are included in headers
-     * -Variables in PublicationProcessingfor which there is a getter are populated once (not per call)
-     *
-     */
-
+    
     //load data (parsetestFTN is just a temp function I threw together from parsetest)
     string filename = "Publications_sample.csv";
     vector<vector<string>> data = parsetestFTN::loadData(filename);
