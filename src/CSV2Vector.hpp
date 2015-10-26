161a5c37a99fca0e99d0629a99aa694d79918731
Alen Badel <abadel@localhost.localdomain>
2015-10-25 19:21:15 -0500

    Baseline For Demo 1

---
+++ src/CSV2Vector.hpp
@@ -0,0 +1,41 @@
+#include <vector>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+/*TODO: These names make sense?? */
+enum publicationField {vm_name, vp_domain, vp_status, vp_type, vs_date, vrole, vauthor, vjn_pi_bt, vtitle};
+
+class myData {
+
+private:
+	vector<vector<string>> *publications, *teaching, *funding, *presentations;
+	vector<vector<string >> parseTeaching(string filePath);
+	vector<vector<string >> parseFunding(string filePath);
+	vector<vector<string >> parsePresentations(string filePath);
+
+public:
+    myData();
+
+    vector<vector<string >> parsePublications(string filePath);
+
+
+	/* Read and create the CSV */
+	bool createParsePublications(string filePath);
+	bool createParseTeaching(string filePath);
+	bool createParseFunding(string filePath);
+	bool createParsePresentations(string filePath);
+
+	/* Check if the respective CSV has been loaded */
+	bool isPublications();
+	bool isTeaching();
+	bool isFunding();
+	bool isPresentations();
+
+	/* Read Publications*/
+#if 0
+	string * readPublications(publicationField, int entryNum);
+#endif
+
+};
---
+++ src/mainwindow.h
@@ -0,0 +1,56 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QComboBox>
+#include <QTreeWidget>
+#include <string>
+#include <vector>
+#include "publicationprocessing/PublicationProcessing.h"
+#include "ui_mainwindow.h"
+#include <QFileDialog>
+#include "../lib/qcustomplot.h"
+
+namespace Ui {
+class MainWindow;
+}
+
+using namespace std;
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+    /**
+     * Draw graph onto windows canvas
+     *
+     * @param w
+     * @param yAxisData
+     * @param title
+     * @return
+     */
+    void makeGraph(QVector<double> yAxisData, QString title , vector<string> barLabels);
+
+private slots:
+    QString on_actionOpen_triggered();
+
+    void on_bntDisplayGraph_clicked();
+
+    QStringList on_btnDates_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    //PublicationProcessing p;
+
+    void initDate_Month(QComboBox *months);
+    void initDate_Years(QComboBox *months);
+    void addTreeRoot(QTreeWidgetItem *treeBranch,QString name, QString description);
+    void addTreeChild(QTreeWidgetItem *parent, QString name, QString description);
+    void processDates();
+};
+
+#endif // MAINWINDOW_H
