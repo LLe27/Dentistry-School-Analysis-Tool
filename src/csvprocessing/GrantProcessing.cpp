bc5c9f367787295dcbf96bbc2a324dcfed5349de
Zain Hemani <zain.hemani7@gmail.com>
2015-12-07 20:02:08 -0500

    Update GrantProcessing.cpp
    Commented

--- src/csvprocessing/GrantProcessing.cpp
+++ src/csvprocessing/GrantProcessing.cpp
@@ -27,15 +27,15 @@ GrantProcessing::GrantProcessing(string filename, int csvtype) : CSVProcessing(f
 
 vector<string> GrantProcessing::getListOfTypes() {
     return types;
-}
+}//Return all types from grantprocessing
 
 vector<string> GrantProcessing::getListOfRoles() {
     return roles;
-}
+}//Return all roles from grantprocessing
 
 vector<int> GrantProcessing::getIndicesType(string type) {
     return getIndicesType(type,allInd);
-}
+}//Return all inidicies for a specific type from grantprocessing
 
 //uses stored type indices
 vector<int> GrantProcessing::getIndicesType(string type, vector<int> indToConsider) {
@@ -53,34 +53,35 @@ vector<int> GrantProcessing::getIndicesType(string type, vector<int> indToConsid
     return getIndicesIntersect( typesIndices.at(indType) , indToConsider );
 }
 
+//Get indicies for all statuses in grant processing
 vector<int> GrantProcessing::getIndicesStatus(string status) {
     return getIndicesStatus(status,allInd);
-}
-
+} 
+//Get indicies for all statuses in grant processing, vector output
 vector<int> GrantProcessing::getIndicesStatus(string status, vector<int> indToConsider) {
     return getIndicesIntersect( getColumnMatch(COLUMN_STATUS,status) , indToConsider );
 }
-
+//Get indicies for all peer reviews in grant processing
 vector<int> GrantProcessing::getIndicesPeerReviewed() {
     return getIndicesPeerReviewed(allInd);
 }
-
+//Get indicies for all peer reviews in grant processing, vector output
 vector<int> GrantProcessing::getIndicesPeerReviewed(vector<int> indToConsider) {
     return getIndicesIntersect( getColumnMatch(COLUMN_PEER_REVIEWED,"True") , indToConsider );
 }
-
+//Get indicies for all industries in grant processing
 vector<int> GrantProcessing::getIndicesIndustry() {
     return getIndicesIndustry(allInd);
 }
-
+//Get indicies for all industries in grant processing, vector output
 vector<int> GrantProcessing::getIndicesIndustry(vector<int> indToConsider) {
     return getIndicesIntersect( getColumnMatch(COLUMN_INDUSTRY,"True") , indToConsider );
 }
-
+//Get indicies for all roles in grant processing
 vector<int> GrantProcessing::getIndicesRole(string role) {
     return getIndicesRole(role,allInd);
 }
-
+//Get indicies for all roles in grant processing, vector output
 vector<int> GrantProcessing::getIndicesRole(string role, vector<int> indToConsider) {
     //get role ind or return empty vector
     int indRole = -1;
@@ -99,7 +100,7 @@ vector<int> GrantProcessing::getIndicesRole(string role, vector<int> indToConsid
 vector<int> GrantProcessing::getIndicesAmount(int minAmount, int maxAmount) {
     return getIndicesAmount(minAmount,maxAmount,allInd);
 }
-
+//GET AMOUNT FOR EACH GRANT 
 double GrantProcessing::getAmount(int index) {
     int Str_index = 0;
    string str = data.at(COLUMN_AMOUNT).at(index);
@@ -112,12 +113,9 @@ double GrantProcessing::getAmount(int index) {
         Str_index++;
    }
 
-   //cout << "Total: " <<str << endl;
-   //cout << "A Total" <<atof(str.c_str()) << endl;
-
    return atof(str.c_str());
 }
-
+//Checks if grant amount is within the bounds at specific indicies 
 vector<int> GrantProcessing::getIndicesAmount(int minAmount, int maxAmount, vector<int> indToConsider) {
     vector<int> result;
     for (int i : indToConsider) {
@@ -138,13 +136,13 @@ vector<int> GrantProcessing::getIndicesTitle(string title, vector<int> indToCons
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /// Private Functions
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+//Fucntion to populate all the types in grant CSV, in unique type vector
 void GrantProcessing::populateTypes() {
     pair<vector<string>,vector<vector<int>>> uniqueType = getUniqueInColumn(COLUMN_TYPE);
     types = uniqueType.first;
     typesIndices = uniqueType.second;
 }
-
+//Fucntion to populate all the roles in grant CSV, in unique type vector
 void GrantProcessing::populateRoles() {
     pair<vector<string>,vector<vector<int>>> uniqueType = getUniqueInColumn(COLUMN_ROLE);
     roles = uniqueType.first;
