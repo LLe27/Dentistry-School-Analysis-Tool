1d1cc54d33306378378ad84152480ec741991b83
Zain Hemani <zain.hemani7@gmail.com>
2015-12-07 20:13:46 -0500

    Update TeachingProcessing.cpp
    Commented

--- src/csvprocessing/TeachingProcessing.cpp
+++ src/csvprocessing/TeachingProcessing.cpp
@@ -21,16 +21,15 @@ TeachingProcessing::TeachingProcessing(string filename, int csvtype) : CSVProces
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /// Public Functions
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+//Returns all types as string in vector 
 vector<string> TeachingProcessing::getListOfTypes() {
     return types;
 }
-
+//Returns all indicies for types within Teaching CSV
 vector<int> TeachingProcessing::getIndicesType(string type) {
     return getIndicesType(type,allInd);
 }
-
-//uses stored type indices
+//Returns all indicies for types within Teaching CSV, vector ouput
 vector<int> TeachingProcessing::getIndicesType(string type, vector<int> indToConsider) {
     //get type ind or return empty vector
     int indType = -1;
@@ -45,19 +44,19 @@ vector<int> TeachingProcessing::getIndicesType(string type, vector<int> indToCon
     //retrurn intersect between known type indicies and indToConsider
     return getIndicesIntersect( typesIndices.at(indType) , indToConsider );
 }
-
+//Returns all indicies for programs within Teaching CSV
 vector<int> TeachingProcessing::getIndicesProgram(string program) {
     return getIndicesProgram(program,allInd);
 }
-
+//Returns all indicies for programs within Teaching CSV, vector ouput
 vector<int> TeachingProcessing::getIndicesProgram(string program, vector<int> indToConsider) {
     return getIndicesIntersect( getColumnMatch(COLUMN_PROGRAM,program) , indToConsider );
 }
-
+//Returns all indicies for hours within Teaching CSV
 vector<int> TeachingProcessing::getIndicesHours(int minHours, int maxHours) {
     return getIndicesHours(minHours,maxHours,allInd);
 }
-
+//Returns all indicies for hours within Teaching CSV, vector ouput
 vector<int> TeachingProcessing::getIndicesHours(int minHours, int maxHours, vector<int> indToConsider) {
     vector<int> result;
     for (int i : indToConsider) {
@@ -65,11 +64,11 @@ vector<int> TeachingProcessing::getIndicesHours(int minHours, int maxHours, vect
     }
     return result;
 }
-
+//Returns all indicies for students within Teaching CSV
 vector<int> TeachingProcessing::getIndicesStudents(int minNumStudents, int maxNumStudents) {
     return getIndicesStudents(minNumStudents,maxNumStudents,allInd);
 }
-
+//Returns all indicies for students within Teaching CSV, vector output
 vector<int> TeachingProcessing::getIndicesStudents(int minNumStudents, int maxNumStudents, vector<int> indToConsider) {
     vector<int> result;
     for (int i : indToConsider) {
@@ -77,11 +76,11 @@ vector<int> TeachingProcessing::getIndicesStudents(int minNumStudents, int maxNu
     }
     return result;
 }
-
+//Get hours function, to get hours at specific index
 double TeachingProcessing::getHours(int index){
      string str = data.at(COLUMN_HOURS).at(index);
      return atof(str.c_str());
-}
+} 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /// Private Functions
@@ -91,4 +90,6 @@ void TeachingProcessing::populateTypes() {
     pair<vector<string>,vector<vector<int>>> uniqueType = getUniqueInColumn(COLUMN_COURSE_TYPE);
     types = uniqueType.first;
     typesIndices = uniqueType.second;
-}
+}//Populate uniqueType with all unique types within teaching CSV
+
+//END OF CLASS
